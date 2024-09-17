f7990671fa6e0125deccc15113323a1be6bc40cb
Cleber-0101 <cleberbatista99@hotmail.com>
2024-09-17 01:09:38 -0300

    Praticando type-Script

---
+++ type.ts
@@ -0,0 +1,13 @@
+const nome = "Cleber"
+const idade = 24 
+const cidade = "São Pualo"
+const noivo = true  
+const servico = "Programador junior"
+
+function nomeDaFuncao(nome : string){
+    console.log("Cleber seja bem vindo ao mundo de type script" , nome)
+}
+
+nomeDaFuncao(nome)
+nomeDaFuncao(idade)
+nomeDaFuncao(cidade)
