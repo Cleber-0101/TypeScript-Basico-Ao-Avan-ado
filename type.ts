b1c5a6cded1a3c4c23b23f5a609f5bdd96a7bb6c
Cleber-0101 <cleberbatista99@hotmail.com>
2024-09-17 01:20:07 -0300

    Somando numeros usando parametros com type script

--- type.ts
+++ type.ts
@@ -1,13 +1,5 @@
-const nome = "Cleber"
-const idade = 24 
-const cidade = "São Pualo"
-const noivo = true  
-const servico = "Programador junior"
-
-function nomeDaFuncao(nome : string){
-    console.log("Cleber seja bem vindo ao mundo de type script" , nome)
+function soma(a:  number , b: number) {
+    return a + b
 }
-
-nomeDaFuncao(nome)
-nomeDaFuncao(idade)
-nomeDaFuncao(cidade)
+//somando numeros usando parametros 
+console.log(soma(2,2))
