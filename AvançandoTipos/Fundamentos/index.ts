b1b9c37c4bdfd8a6d98df7da6d1808f51ea487d9
Cleber-0101 <cleberbatista99@hotmail.com>
2024-09-17 23:53:47 -0300

    estudando sobre mudar o tipo de uma variavel

---
+++ AvançandoTipos/Fundamentos/index.ts
@@ -0,0 +1,7 @@
+//tipo inference e depois transformando o tipo int em tipo string 
+const nn: number = 558;
+
+const nn1 = nn.toString();
+
+const resultado: string = `eu sou zica do type${nn1}`;
+console.log(resultado);
